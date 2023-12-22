d5a7b23bfc60b223e3d94aafc662d2fff67caf8b
Keila <keilaoliveira.copy@gmail.comm>
2023-12-22 15:38:29 -0300

    feat: criation endpoint GET

--- src/main/java/com/api/fleetmanagement/controller/TaxisController.java
+++ src/main/java/com/api/fleetmanagement/controller/TaxisController.java
@@ -3,6 +3,10 @@ package com.api.fleetmanagement.controller;
 import com.api.fleetmanagement.models.TaxisModel;
 import com.api.fleetmanagement.repository.TaxisRepository;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,6 +25,14 @@ public class TaxisController {
     TaxisRepository taxisRepository;
 
     @Operation(summary = "Get all taxis from database")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the book",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = TaxisModel.class)) }),
+            @ApiResponse(responseCode = "400", description = "Invalid id supplied",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Taxis not found",
+                    content = @Content) })
     @GetMapping("/taxis")
     public ResponseEntity<Page<TaxisModel>> getAllTaxis(Pageable pageable) {
         return ResponseEntity.status(HttpStatus.OK).body(taxisRepository.findAll(pageable));
